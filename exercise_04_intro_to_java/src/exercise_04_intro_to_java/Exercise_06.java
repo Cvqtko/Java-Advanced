c18d03d59ae64923867072e5a8c6848d7461e8d0
Cvqtko <cvqtkokirilov@gmail.com>
2020-01-13 20:22:10 +0200

    More exercises on intro to java

--- exercise_04_intro_to_java/src/exercise_04_intro_to_java/Exercise_06.java
+++ exercise_04_intro_to_java/src/exercise_04_intro_to_java/Exercise_06.java
@@ -1,5 +1,28 @@
 package exercise_04_intro_to_java;
 
-public class Exercise_06 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Exercise_06 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(reader.readLine());
+		int sum;
+		int subtract;
+
+		for (int i = 1; i <= 20; i++) {
+			for (int j = 1; j <= 20; j++) {
+				subtract = i - j;
+				sum = i + j;
+				if (sum == n) {
+					System.out.println(String.format("%s + %s = %s", i, j, n));
+				}
+				if (subtract == n) {
+					System.out.println(String.format("%s - %s = %s", i, j, n));
+				}
+			}
+		}
+	}
 }
--- exercise_04_intro_to_java/src/exercise_04_intro_to_java/Exercise_07.java
+++ exercise_04_intro_to_java/src/exercise_04_intro_to_java/Exercise_07.java
@@ -1,5 +1,30 @@
 package exercise_04_intro_to_java;
 
-public class Exercise_07 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Exercise_07 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+		String[] input = reader.readLine().split("\\s+");
+		char[] first = input[0].toCharArray();
+		char[] second = input[1].toCharArray();
+
+		int result = 0;
+
+		for (int i = 0; i < Math.max(first.length, second.length); i++) {
+			if (i < first.length && i < second.length) {
+				result += first[i] * second[i];
+			} else if (i < first.length) {
+				result += first[i];
+			} else if (i < second.length) {
+				result += second[i];
+			}
+		}
+
+		System.out.println(result);
+	}
 }
